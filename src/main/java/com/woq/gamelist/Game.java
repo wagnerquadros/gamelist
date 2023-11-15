8545b0a9feccbbdd1193389d8f39723bdf41c7e4
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-11-15 10:05:49 -0300

    game object

--- src/main/java/com/woq/gamelist/Game.java
+++ src/main/java/com/woq/gamelist/Game.java
@@ -16,23 +16,27 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
     }
 
     public Game(Long id, String title, Integer year, String genre,
-                String plataform, Double score, String imgUrl,
+                String platforms, Double score, String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -71,12 +75,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlataform(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
